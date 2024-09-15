796f0a87582bbf5d7134be885d4380c42a358801
AbnerTeng <abnerteng16@gmail.com>
2024-09-15 21:42:42 +0800

    fix: pydantic issue

--- app/api/make_qr.py
+++ app/api/make_qr.py
@@ -2,8 +2,8 @@ import qrcode
 
 
 class QRCodeGenerator:
-    def __init__(self, content: str):
-        self.content = content
+    def __init__(self, url: str):
+        self.url = url
         self.qr = qrcode.QRCode(
             version=None,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -12,8 +12,7 @@ class QRCodeGenerator:
         )
 
     def get_qrcode(self):
-        self.qr.add_data(self.content)
+        self.qr.add_data(self.url)
         self.qr.make(fit=True)
         image = self.qr.make_image(fill_color="black", back_color="white")
         return image
-
